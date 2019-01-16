1a4761b31fa2085cc4685fc0cb66c70e1cb73b7c
Iamtheapexpredator <danielsonlukea@gmail.com>
2019-01-15 19:40:50 -0700

    added properties to the placeSchema

--- src/models/place.js
+++ src/models/place.js
@@ -1,4 +1,4 @@
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 
 var placeSchema = new mongoose.Schema({
   location: String,
@@ -28,28 +28,67 @@ var placeSchema = new mongoose.Schema({
   },
   hourlySummary: String,
   hourlyIcon: String,
-  hourly: [{
-    time: Number,
-    nearestStormDistance: Number,
-    precipIntensity: Number,
-    precipIntensityError: Number,
-    precipProbability: Number,
-    precipType: String,
-    temperature: Number,
-    apparentTemperature: Number,
-    dewPoint: Number,
-    humidity: Number,
-    pressure: Number,
-    windSpeed: Number,
-    windGust: Number,
-    windBearing: Number,
-    cloudCover: Number,
-    uvIndex: Number,
-    visibility: Number,
-    ozone: Number
-  }]
+  hourly: [
+    {
+      time: Number,
+      nearestStormDistance: Number,
+      precipIntensity: Number,
+      precipIntensityError: Number,
+      precipProbability: Number,
+      precipType: String,
+      temperature: Number,
+      apparentTemperature: Number,
+      dewPoint: Number,
+      humidity: Number,
+      pressure: Number,
+      windSpeed: Number,
+      windGust: Number,
+      windBearing: Number,
+      cloudCover: Number,
+      uvIndex: Number,
+      visibility: Number,
+      ozone: Number
+    }
+  ],
+  dailySummary: String,
+  dailyIcon: String,
+  daily: [
+    {
+      time: Number,
+      sunriseTime: Number,
+      sunsetTime: Number,
+      precipIntensity: Number,
+      precipIntensityMax: Number,
+      precipIntensityMaxTime: Number,
+      precipProbability: Number,
+      precipType: String,
+      temperatureHigh: Number,
+      temperatureHighTime: Number,
+      temperatureLow: Number,
+      temperatureLowTime: Number,
+      apparentTemperatureHigh: Number,
+      apparentTemperatureHighTime: Number,
+      apparentTemperatureLow: Number,
+      apparentTemperatureLowTime: Number,
+      humidity: Number,
+      windSpeed: Number,
+      windGust: Number,
+      windGustTime: Number,
+      windBearing: Number,
+      cloudCover: Number,
+      visibility: Number,
+      temperatureMin: Number,
+      temperatureMinTime: Number,
+      temperatureMax: Number,
+      temperatureMaxTime: Number,
+      apparentTemperatureMin: Number,
+      apparentTemperatureMinTime: Number,
+      apparentTemperatureMax: Number,
+      apparentTemperatureMaxTime: Number
+    }
+  ]
 });
 
-var Place = mongoose.model('Place', placeSchema);
+var Place = mongoose.model("Place", placeSchema);
 
 module.exports = Place;
